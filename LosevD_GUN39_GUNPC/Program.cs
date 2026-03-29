319155585c2c19666b3471209d7b393ff1357200
d0kdenya <denden2100.nn@mail.ru>
2026-03-29 10:50:30 +0300

    Added calculator

--- LosevD_GUN39_GUNPC/Program.cs
+++ LosevD_GUN39_GUNPC/Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+   static void Main(string[] args)
+   {
+      Console.Write("Input first value: ");
+
+      if (!Int32.TryParse(Console.ReadLine(), out int a))
+      {
+         Console.WriteLine("Not a number!");
+         return;
+      }
+
+      Console.Write("Input second value: ");
+
+      if (!Int32.TryParse(Console.ReadLine(), out int b))
+      {
+         Console.WriteLine("Not a number!");
+         return;
+      }
+
+      Console.Write("Input operation (+, -, *, /, %, &, |, ^): ");
+      var s = Console.ReadLine();
+
+      if (s.Length != 1)
+      {
+         Console.WriteLine("Wrong operation!");
+         return;
+      }
+
+      switch (s[0])
+      {
+         case '+':
+            Console.WriteLine("Binary result of {0} + {1} = {2}", a, b, Convert.ToString(a + b, 2));
+            Console.WriteLine("Decimal result of {0} + {1} = {2}", a, b, Convert.ToString(a + b, 10));
+            Console.WriteLine("Hexadecimal result of {0} + {1} = {2}", a, Convert.ToString(a + b, 16));
+            break;
+         case '-':
+            Console.WriteLine("Binary result of {0} - {1} = {2}", a, b, Convert.ToString(a - b, 2));
+            Console.WriteLine("Decimal result of {0} - {1} = {2}", a, b, Convert.ToString(a - b, 10));
+            Console.WriteLine("Hexadecimal result of {0} - {1} = {2}", a, b, Convert.ToString(a - b, 16));
+            break;
+         case '*':
+            Console.WriteLine("Binary result of {0} * {1} = {2}", a, b, Convert.ToString(a * b, 2));
+            Console.WriteLine("Decimal result of {0} * {1} = {2}", a, b, Convert.ToString(a * b, 10));
+            Console.WriteLine("Hexadecimal result of {0} * {1} = {2}", a, b, Convert.ToString(a * b, 16));
+            break;
+         case '/':
+            Console.WriteLine("Binary result of {0} / {1} = {2}", a, b, Convert.ToString(a / b, 2));
+            Console.WriteLine("Decimal result of {0} / {1} = {2}", a, b, Convert.ToString(a / b, 10));
+            Console.WriteLine("Hexadecimal result of {0} / {1} = {2}", a, b, Convert.ToString(a / b, 16));
+            break;
+         case '%':
+            Console.WriteLine("Binary result of {0} % {1} = {2}", a, b, Convert.ToString(a % b, 2));
+            Console.WriteLine("Decimal result of {0} % {1} = {2}", a, b, Convert.ToString(a % b, 10));
+            Console.WriteLine("Hexadecimal result of {0} % {1} = {2}", a, b, Convert.ToString(a % b, 16));
+            break;
+         case '&':
+            Console.WriteLine("Binary result of {0} & {1} = {2}", a, b, Convert.ToString(a & b, 2));
+            Console.WriteLine("Decimal result of {0} & {1} = {2}", a, b, Convert.ToString(a & b, 10));
+            Console.WriteLine("Hexadecimal result of {0} & {1} = {2}", a, b, Convert.ToString(a & b, 16));
+            break;
+         case '|':
+            Console.WriteLine("Binary result of {0} | {1} = {2}", a, b, Convert.ToString(a | b, 2));
+            Console.WriteLine("Decimal result of {0} | {1} = {2}", a, b, Convert.ToString(a | b, 10));
+            Console.WriteLine("Hexadecimal result of {0} | {1} = {2}", a, b, Convert.ToString(a | b, 16));
+            break;
+         case '^':
+            Console.WriteLine("Binary result of {0} ^ {1} = {2}", a, b, Convert.ToString(a ^ b, 2));
+            Console.WriteLine("Decimal result of {0} ^ {1} = {2}", a, b, Convert.ToString(a ^ b, 10));
+            Console.WriteLine("Hexadecimal result of {0} ^ {1} = {2}", a, b, Convert.ToString(a ^ b, 16));
+            break;
+         default:
+            Console.WriteLine("Wrong sing!");
+            return;
+      }
+   }
+}
